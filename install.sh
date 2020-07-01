8a903d6ffd828747397328e489ac430899af863c
lcenchew <lcenchew@outlook.com>
2020-07-01 09:53:04 +0800

    Update install.sh
    Install phpmyadmin with docker instead

--- install.sh
+++ install.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-echo "Installing PHPMyAdmin"
-
-mkdir public
-cd public
-git clone https://github.com/phpmyadmin/phpmyadmin.git
-cd phpmyadmin
-composer update --no-dev
-yarn install
-cp config.sample.inc.php config.inc.php
-# Remember to set AllowNoPassword (line 32 in config.inc.php) to false
-
-echo "Done"
-source ~/.bashrc
+# source ~/.bashrc
